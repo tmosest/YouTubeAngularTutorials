6bc72cce3632a33f7e5745728c7fe8d99cada408
tmosest <tmosest@gmail.com>
2017-01-21 13:58:58 -0500

    Finsihed video_1e.

--- calculator/js/calculator.controller.js
+++ calculator/js/calculator.controller.js
@@ -6,23 +6,31 @@ angular.module('CalculatorApp', [])
         
         // Here is an example of adding a variable to the scope.
         $scope.title = 'AngularJS Calculator';
-        
+        $scope.a = 10;
+        $scope.b = 5;
+        $scope.operator = '+';
         // Here is an example of adding a method to the scope.
         $scope.result = function () {
-            if ($scope.operator == '+') {
-                return $scope.a + $scope.b;
-            }
-            if ($scope.operator == '-') {
-                return $scope.a - $scope.b;
-            }
-            if ($scope.operator == '*') {
-                return $scope.a * $scope.b;
-            }
-            if ($scope.operator == '/') {
-                return $scope.a / $scope.b;
-            }
-            if ($scope.operator == '%') {
-                return $scope.a % $scope.b;
-            }
+            return calculate($scope.a, $scope.b, $scope.operator);
         };
+        // Private functions
+        function calculate(a, b, operator)
+        {
+            if (operator == '+') {
+                return a + b;
+            }
+            if (operator == '-') {
+                return a - b;
+            }
+            if (operator == '*') {
+                return a * b;
+            }
+            if (operator == '/') {
+                return a / b;
+            }
+            if (operator == '%') {
+                return a % b;
+            }
+            return a + b;
+        }
     });

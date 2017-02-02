9d89d9cedfe53ee5e8ab5350a25d3df7673dabe2
Tyler Moses <tmosest@gmail.com>
2017-02-02 18:43:18 -0500

    Forgot to update this for end video

--- calculator/js/calculator.controller.js
+++ calculator/js/calculator.controller.js
@@ -5,17 +5,16 @@ angular.module('CalculatorApp', [])
     
     CalculatorController.$inject = ['$scope'];
 
-    function CalculatorController($scope) {
-        // $scope is the old way of defining variables and methods on the controller.
-        
+    function CalculatorController() {
+        var vm = this;        
         // Here is an example of adding a variable to the scope.
-        $scope.title = 'AngularJS Calculator';
-        $scope.a = 10;
-        $scope.b = 5;
-        $scope.operator = '+';
+        vm.title = 'AngularJS Calculator';
+        vm.a = 10;
+        vm.b = 5;
+        vm.operator = '+';
         // Here is an example of adding a method to the scope.
-        $scope.result = function () {
-            return calculate($scope.a, $scope.b, $scope.operator);
+        vm.result = function () {
+            return calculate(vm.a, vm.b, vm.operator);
         };
         // Private functions
         function calculate(a, b, operator)

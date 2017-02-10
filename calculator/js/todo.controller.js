cab5b254483a8fdffde288a1428aebf1c38477af
tmosest <tmosest@gmail.com>
2017-02-09 22:23:58 -0500

    Fin v4

--- calculator/js/todo.controller.js
+++ calculator/js/todo.controller.js
@@ -11,13 +11,37 @@ angular.module('CalculatorApp')
         vm.count = 0;
         vm.todos = []; // empty array
         vm.task = {}; // empty object
-        // methods
+        /** 
+         * Method to add items from our task list 
+         */
         vm.add = function () {
             vm.count++;
             vm.task = {
                 id: vm.count,
-                title: vm.taskTitle
+                title: vm.taskTitle,
+                checked: false,
+                important: false
             };
             vm.todos.push(vm.task);
+            // Reset the taskTitle:
+            vm.taskTitle = null;
+        };
+        /** 
+         * Method to delete items from our task list 
+         */
+        vm.delete = function(id) {
+            if(id > 0) {
+                index = -1;
+                for(var i = 0; i < vm.todos.length; i++) {
+                    if(vm.todos[i].id == id) {
+                        index = i;
+                        break;
+                    }
+                }
+                if(index != -1) {
+                    vm.todos.splice(index, 1);
+                    vm.count--;
+                }
+            }
         };
     }
